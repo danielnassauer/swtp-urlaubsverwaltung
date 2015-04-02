ddfd56152647fbfde9921512ac651a4665961e86
danielnassauer <daniel.nassauer@mni.thm.de>
2015-04-02 18:30:41 +0200

    added user rights table creation and deletion

--- admin.php
+++ admin.php
@@ -1,11 +1,11 @@
 <?php
-require_once dirname(__FILE__).'/server/db/DBCreator.php';
-require_once dirname(__FILE__).'/server/db/HolidayRequests.php';
+require_once dirname ( __FILE__ ) . '/server/db/DBCreator.php';
+require_once dirname ( __FILE__ ) . '/server/db/HolidayRequests.php';
 
 if (isset ( $_POST ['create_holidayrequests_table'] )) {
 	try {
 		DBCreator::createHolidayRequestsTable ();
-		echo '<div class="alert alert-success" role="alert">Urlaubsverwaltung DB erfolgreich erstellt!</div>';
+		echo '<div class="alert alert-success" role="alert">HolidayRequests-Table erfolgreich erstellt!</div>';
 	} catch ( Exception $e ) {
 		echo '<div class="alert alert-danger" role="alert">' . $e->getMessage () . '</div>';
 	}
@@ -14,7 +14,25 @@ if (isset ( $_POST ['create_holidayrequests_table'] )) {
 if (isset ( $_POST ['delete_holidayrequests_table'] )) {
 	try {
 		DBCreator::deleteHolidayRequestsTable ();
-		echo '<div class="alert alert-success" role="alert">Urlaubsverwaltung DB erfolgreich gelöscht!</div>';
+		echo '<div class="alert alert-success" role="alert">HolidayRequests-Table erfolgreich gelöscht!</div>';
+	} catch ( Exception $e ) {
+		echo '<div class="alert alert-danger" role="alert">' . $e->getMessage () . '</div>';
+	}
+}
+
+if (isset ( $_POST ['create_userrights_table'] )) {
+	try {
+		DBCreator::createUserRightsTable ();
+		echo '<div class="alert alert-success" role="alert">UserRights-Table erfolgreich erstellt!</div>';
+	} catch ( Exception $e ) {
+		echo '<div class="alert alert-danger" role="alert">' . $e->getMessage () . '</div>';
+	}
+}
+
+if (isset ( $_POST ['delete_userrights_table'] )) {
+	try {
+		DBCreator::deleteUserRightsTable ();
+		echo '<div class="alert alert-success" role="alert">UserRights-Table erfolgreich gelöscht!</div>';
 	} catch ( Exception $e ) {
 		echo '<div class="alert alert-danger" role="alert">' . $e->getMessage () . '</div>';
 	}
@@ -37,37 +55,59 @@ if (isset ( $_POST ['delete_holidayrequests_table'] )) {
 </head>
 
 <body>
-	<div class="container">
+	<form action="admin.php" method="POST">
+		<div class="container">
 
 
-		<!-- HOLIDAYREQUESTS DB -->
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h3 class="panel-title">Urlaubsverwaltung DB</h3>
-			</div>
-			<div class="panel-body">
-				<form action="admin.php" method="POST">
+			<!-- HOLIDAYREQUESTS TABLE -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Urlaubsanträge</h3>
+				</div>
+				<div class="panel-body">
 					<button type="submit" class="btn btn-default"
 						name="create_holidayrequests_table">Tabelle erstellen</button>
-						<button type="submit" class="btn btn-default"
+					<button type="submit" class="btn btn-default"
 						name="delete_holidayrequests_table">Tabelle löschen</button>
-				</form>
-				
-				<table>
-				<tr><th>HolidayRequests</th></tr>
-				<?php 				
-					foreach (HolidayRequests::getRequests() as $request){
+
+					<table>
+						<tr>
+							<th>HolidayRequests</th>
+						</tr>
+				<?php
+				try {
+					foreach ( HolidayRequests::getRequests () as $request ) {
 						echo "<tr>";
-						echo "<td>".$request->toJSON()."</td>";
+						echo "<td>" . $request->toJSON () . "</td>";
 						echo "</tr>";
 					}
+				} catch ( Exception $e ) {
+					echo '<tr><td colspan="10">' . $e->getMessage () . '</td></tr>';
+				}
 				?>
 				</table>
+				</div>
+			</div>
+
+			<!-- USERRIGHTS TABLE -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Benutzerrechte</h3>
+				</div>
+				<div class="panel-body">
+
+					<button type="submit" class="btn btn-default"
+						name="create_userrights_table">Tabelle erstellen</button>
+					<button type="submit" class="btn btn-default"
+						name="delete_userrights_table">Tabelle löschen</button>
+
+				</div>
 			</div>
 		</div>
 
-	</div>
 
+		</div>
+	</form>
 </body>
 
 </html>
--- server/db/DBCreator.php
+++ server/db/DBCreator.php
@@ -32,10 +32,39 @@ class DBCreator {
 	}
 
 	public static function deleteHolidayRequestsTable() {
+		self::deleteTable ( "HolidayRequests" );
+	}
+
+	public static function createUserRightsTable() {
+		global $mysql_servername, $mysql_username, $mysql_password, $db_holiday;
+		
+		$sql = "CREATE TABLE UserRights (
+				user INT(11) UNSIGNED PRIMARY KEY,
+				rights INT(11)				
+				)";
+		
+		self::createHolidayDB ();
+		$conn = new mysqli ( $mysql_servername, $mysql_username, $mysql_password, $db_holiday );
+		if (mysqli_connect_errno ()) {
+			throw new Exception ( "MySQL connection failed: " . mysqli_connect_error () );
+		}
+		
+		if (! $conn->query ( $sql )) {
+			throw new Exception ( "Could not create user rights table: " . $conn->error );
+		}
+		
+		$conn->close ();
+	}
+
+	public static function deleteUserRightsTable() {
+		self::deleteTable ( "UserRights" );
+	}
+
+	private static function deleteTable($table) {
 		global $mysql_servername, $mysql_username, $mysql_password, $db_holiday;
 		$conn = new mysqli ( $mysql_servername, $mysql_username, $mysql_password, $db_holiday );
-		if (! $conn->query ( "DROP TABLE IF EXISTS HolidayRequests" )) {
-			throw new Exception ( "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error );
+		if (! $conn->query ( "DROP TABLE IF EXISTS " . $table )) {
+			throw new Exception ( "Table deletion failed: (" . $mysqli->errno . ") " . $mysqli->error );
 		}
 	}
 

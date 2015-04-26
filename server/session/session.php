1907c7859f6958d30431bea0589df70e8370bd04
danielnassauer <daniel.nassauer@mni.thm.de>
2015-04-26 18:03:25 +0200

    changed authentication: using existing authentication

--- server/session/session.php
+++ server/session/session.php
@@ -1,8 +1,3 @@
-<?php
-session_start ();
-require_once dirname ( __FILE__ ) . '/../db/Authentication.php';
-if (isset ( $_POST ["user"] ) && isset ( $_POST ["password"] )) {
-	// sign in and set user id
-	$_SESSION ["user"] = Authentication::signIn ( $_POST ["user"], $_POST ["password"] );
-}
+<?php 
+//DUMMY
 ?>

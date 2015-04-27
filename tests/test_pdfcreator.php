535863461184b158ae76b9d7915f09f1a1fa6930
RaoulRails <raoul.yemetio.ngoula@mni.thm.de>
2015-04-27 14:34:22 +0200

    Test zur PDF Erzeugung

--- tests/test_pdfcreator.php
+++ tests/test_pdfcreator.php
@@ -3,9 +3,9 @@ require '../server/PDFCreator.php';
 require '../server/model/HolidayRequest.php';
 require '../server/model/Person.php';
 
-$person = new Person(42, "Max", "Mustermann", "Verkauf", False, 50, 1);
+$person = new Person(42, "Max", "Mustermann", "Verkauf", False, 50, 1,false);
 
 //abgelehnter urlaubsantrag
-$holiday_request = new HolidayRequest(1, 1428105600, 1428278400, $person, array(), 2, 3,"kein Vertreter!");
+$holiday_request = new HolidayRequest(1, 1428105600, 1428278400, $person, array(), 1, 3,"kein Vertreter!");
 PDFCreator::writePDF($holiday_request, "../pdf/".$person->getForename().".pdf");
 

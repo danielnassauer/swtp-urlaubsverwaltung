8ed710636092bc39706ea35891ad5d2f57266f94
danielnassauer <daniel.nassauer@mni.thm.de>
2015-05-04 19:59:42 +0200

    fixed holidaycalculator

--- server/HolidayCalculator.php
+++ server/HolidayCalculator.php
@@ -5,7 +5,7 @@ class HolidayCalculator {
 
 	/**
 	 * Berechnet die Anzahl der verbrauchten Urlaubstage.
-	 * 
+	 *
 	 * @param
 	 *        	start Start-Datum des Urlaubs als unix-timestamp
 	 * @param
@@ -13,42 +13,33 @@ class HolidayCalculator {
 	 * @return Anzahl der verbrauchten Urlaubstage
 	 */
 	public static function calculateHolidays($start, $end) {
-		$differenz = $end - $start;
-		$tag = floor ( $differenz / (3600 * 24) ) + 1; // ingesamt Tage
-		$count = 0; // Zählt Samstage und Sonntage
-		$feiertage = self::feiertageInSE ( $start, $end );
+		$holidays = Holidays::getHolidays (); // feiertage
+		$count = 0;
 		
-		$iter = 24 * 60 * 60; // Ein Tag in Sekunden
-		
-		for($i = $start; $i <= $end; $i = $i + $iter) {
-			if (Date ( 'D', $i ) == 'Sat' || Date ( 'D', $i ) == 'Sun') {
+		for($day = $start; $day <= $end; $day += 24 * 60 * 60) {
+			if (! self::isHoliday ( $day, $holidays ) && ! self::isWeekend ( $day )) {
 				$count ++;
 			}
 		}
-		$urlaubstage = ($tag - ($feiertage + $count));
 		
-		return $urlaubstage;
+		return $count;
 	}
 
-	private static function feiertageInSE($start, $end) {
-		$holidays = Holidays::getHolidays (); // feiertage
-		$count = 0; // Anzahl der Feiertage von Montag bis Freitag
-		
-		foreach ( $holidays as $value ) {
-			if (strcmp(date("d.m.Y",$value->day), date("d.m.Y",$start)) == 0) {
-				while ( strcmp(date("d.m.Y",$value->day), date("d.m.Y",$end)) != 0 ) {
-					$datum = date ( "d.m.Y", $value->day );
-					$wochentag = $datum ['wday'];
-					// Prüfen, ob Werkstag
-					if ($wochentag != 0 && $wochentag != 6) {
-						$count ++;
-					}
-					
-					$value = $value->day + 86400;
-				}
+	private static function isHoliday($timestamp, $holidays) {
+		foreach ( $holidays as $holiday ) {
+			if (self::isSameDay ( $holiday->day, $timestamp )) {
+				return true;
 			}
 		}
-		return $count;
+		return false;
+	}
+
+	private static function isWeekend($timestamp) {
+		return Date ( 'D', $timestamp ) == 'Sat' || Date ( 'D', $timestamp ) == 'Sun';
+	}
+
+	private static function isSameDay($timestamp1, $timestamp2) {
+		return strcmp ( date ( "d.m.Y", $timestamp1 ), date ( "d.m.Y", $timestamp2 ) ) == 0;
 	}
 }
 

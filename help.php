04b5d5295c096533efbdfd9a67d98d3e6472bf2b
danielnassauer <daniel.nassauer@mni.thm.de>
2015-05-06 14:00:22 +0200

    bug fixed

--- help.php
+++ help.php
@@ -27,7 +27,7 @@
 		}
 		if(!user.is_admin){
 			$("#admin_ion").addClass('hidden');
-		}
+		}});
 </script>
 </head>
 
@@ -39,11 +39,11 @@
 			</div>
 			<div>
 				<ul class="nav navbar-nav">
-					<li><a href="index.php"><span class="ion-grid">
-								Übersicht</a></li>
+					<li><a href="index.php"><span class="ion-grid"> Übersicht</a></li>
 					<li><a href="my.php"><span class="ion-home"></span> Mein Kalender</a></li>
 					<li><a href="requests.php"><span class="ion-clipboard"> Anfragen</a></li>
-					<li class="active"><a href="help.php"><span class="ion-help-circled"> Hilfe</a></li>
+					<li class="active"><a href="help.php"><span
+							class="ion-help-circled"> Hilfe</a></li>
 					<li><a href="admin.php"><span class="ion-gear-b" id="admin_ion">
 								Admin</a></li>
 				</ul>
@@ -60,28 +60,41 @@
 		$("#loginPerson").html(rows);
 	</script>
 	<div style="padding-top: 70px"></div>
-	
-		<div class="container">
+
+	<div class="container">
 		<h1>Übersicht</h1>
-		<p>Hier sehen Sie alle bestätigten Urlaubsanfragen und Krankheitsfälle.
-		Über den Filter können Sie oben eine Abteilung auswählen, um sich nur die Anfragen der gewählten Abteilung anzeigen zu lassen.
-		Im Kalender können sie außerdem zwischen der Jahres- und der Monatsansicht wechseln. Krankheitsfälle und Außendienstmitarbeiter werden 
-		mit anderen Farben gekennzeichnet.</p> 
-		
+		<p>Hier sehen Sie alle bestätigten Urlaubsanfragen und
+			Krankheitsfälle. Über den Filter können Sie oben eine Abteilung
+			auswählen, um sich nur die Anfragen der gewählten Abteilung anzeigen
+			zu lassen. Im Kalender können sie außerdem zwischen der Jahres- und
+			der Monatsansicht wechseln. Krankheitsfälle und
+			Außendienstmitarbeiter werden mit anderen Farben gekennzeichnet.</p>
+
 		<h1>Mein Kalender</h1>
 		<h2>Außendienst und restliche Urlaubstage</h2>
-		<p>Im oberen Teil können Sie ihre restlichen Urlaubstage einsehen und wählen, ob Sie sich zurzeit im Außendiest befinden.</p>
-		<h2>Neuen Antrag erstellen</h2>		
-		<p>Im Kalender können Sie einen neuen Antrag stellen. Ziehen Sie dafür mit der Maus vom gewünschten Start-Tag bis zum End-Tag. Im sich daraufhin öfnnenden Dialog, können Sie die Art Ihres Antrages und eventuelle Vertretungen wählen.</p>
-		<h2>Gestellte Anträge einsehen, ändern und stornieren</h2>		
-		<p>Auf der rechten Seite sehen Sie ihre gestellten Anträge sortiert nach noch unbeantworteten, angenommenen und abgelehnten Anträgen.</p>
-		<p>Angenommene Anträge können Sie storniern oder sich eine PDF-Bestätigung erzeugen lassen. Noch nicht bestätigte Anträge können Sie bearbeiten oder stornieren.</p>
-		
+		<p>Im oberen Teil können Sie ihre restlichen Urlaubstage einsehen und
+			wählen, ob Sie sich zurzeit im Außendiest befinden.</p>
+		<h2>Neuen Antrag erstellen</h2>
+		<p>Im Kalender können Sie einen neuen Antrag stellen. Ziehen Sie dafür
+			mit der Maus vom gewünschten Start-Tag bis zum End-Tag. Im sich
+			daraufhin öfnnenden Dialog, können Sie die Art Ihres Antrages und
+			eventuelle Vertretungen wählen.</p>
+		<h2>Gestellte Anträge einsehen, ändern und stornieren</h2>
+		<p>Auf der rechten Seite sehen Sie ihre gestellten Anträge sortiert
+			nach noch unbeantworteten, angenommenen und abgelehnten Anträgen.</p>
+		<p>Angenommene Anträge können Sie storniern oder sich eine
+			PDF-Bestätigung erzeugen lassen. Noch nicht bestätigte Anträge können
+			Sie bearbeiten oder stornieren.</p>
+
 		<h1>Anfragen</h1>
 		<h2>Vertretungsanfragen bearbeiten</h2>
-		<p>In der oberen Liste sehen Sie Vertretungsanfragen von anderen Mitarbeitern an Sie. Sie können der Vertretung zustimmen, sie ablehnen, oder eine andere Person als Vertretun vorschlagen.</p>		
+		<p>In der oberen Liste sehen Sie Vertretungsanfragen von anderen
+			Mitarbeitern an Sie. Sie können der Vertretung zustimmen, sie
+			ablehnen, oder eine andere Person als Vertretun vorschlagen.</p>
 		<h2>Anfragen der Mitarbeiter beantworten</h2>
-		<p>Als Abteilungsleiter oder Geschäftsleiter können Sie die Anträge Ihrer Mitarbeiter beantworten. Die Anträge stehen in der unteren Liste und können entweder angenommen, abgelehnt, oder verlegt werden.</p>		
+		<p>Als Abteilungsleiter oder Geschäftsleiter können Sie die Anträge
+			Ihrer Mitarbeiter beantworten. Die Anträge stehen in der unteren
+			Liste und können entweder angenommen, abgelehnt, oder verlegt werden.</p>
 	</div>
 	<!-- /container -->
 	</div>
